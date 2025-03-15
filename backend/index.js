cf7a101f2db7838d03d61945d18fea2d4519a3a7
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2025-03-15 19:45:52 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import { connect } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
 import { app,server,io } from "./src/lib/Socket.js";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json({limit:"70mb"}));
@@ -17,9 +18,19 @@ app.use(
   })
 );
 app.use(cookieParser());
+const __dirName=path.resolve();
 
 app.use("/api/auth", authRoutes);
-app.use('/api/message',messageRoutes)
+app.use('/api/message',messageRoutes);
+
+if(process.env.NODE_ENV==="production")
+{
+  app.use(express.static(path.join(__dirName+'..frontend/dist')));
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirName,"../frontend","dist","index.html"));
+  })
+}
 
 server.listen(PORT, function () {
   console.log("Server is running on 5001");
