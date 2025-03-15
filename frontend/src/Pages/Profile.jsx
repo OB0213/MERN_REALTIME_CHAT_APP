e3c07565778b8eb6ba66b7ba709312dd30adac0a
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2025-03-15 19:56:53 +0530

    deployment changes

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../Store/useAuthStore";
 import profileImg from "../assets/profileImg.jpg";
 import { Camera, User, Mail } from "lucide-react";
 
