474f71d8c88bf487386f620f483946d028d540f8
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2025-03-15 19:52:54 +0530

    deployment changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Signup from "./Pages/Signup";
 import Settings from "./Pages/Settings";
 import Profile from "./Pages/Profile";
 import Navbar from "./Components/Navbar";
-import { useAuthStore } from "./store/useAuthStore";
+import { useAuthStore } from "./Store/useAuthStore"
 import { Loader } from "lucide-react";
 import {Toaster} from 'react-hot-toast';
 import { useThemeStore } from "./Store/useThemeStore";
