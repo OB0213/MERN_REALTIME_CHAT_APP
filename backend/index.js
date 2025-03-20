761cc6a6bc3d1041216e193eeea29b58b9d6a5a0
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2025-03-20 20:04:16 +0530

    Refresh Data

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ if(process.env.NODE_ENV==="production")
   app.use(express.static(path.join(__dirName+'..frontend/dist')));
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirName,"../frontend","dist","index.html"));
+    res.sendFile(path.join(__dirName,"../frontend","index.html"));
   })
 }
 
